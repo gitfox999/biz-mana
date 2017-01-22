595209830de4ed378054f207879b2f0269e75343
cxy <490437679@qq.com>
2017-01-22 14:40:05 +0800

    ewfs

--- src/me/imvc/app/controllers/mana/Flow2Controller.java
+++ src/me/imvc/app/controllers/mana/Flow2Controller.java
@@ -1,13 +1,29 @@
 package me.imvc.app.controllers.mana;
 
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts2.ServletActionContext;
+
+import me.imvc.app.entities.Flow;
 import me.imvc.app.models.mana.FlowModel;
+import me.imvc.app.models.mana.MemberModel;
 import me.imvc.core.BaseController;
 
 public class Flow2Controller extends BaseController {
 	private FlowModel flowModel;
-
+	private MemberModel memberModel;
 	public FlowModel getFlowModel() {
 		return flowModel;
+	}
+
+	public MemberModel getMemberModel() {
+		return memberModel;
+	}
+
+	public void setMemberModel(MemberModel memberModel) {
+		this.memberModel = memberModel;
 	}
 
 	public void setFlowModel(FlowModel flowModel) {
@@ -15,8 +31,13 @@ public class Flow2Controller extends BaseController {
 	}
 
 	@Override
-	public String index() {
+	public String index(){
 		// TODO Auto-generated method stub
+		String id =flowModel.getMemId();
+		String [] b={id};
+		if(flowModel.getSearchParams()!=null){
+			flowModel.getSearchParams().put("and_memId_eq", b);
+		}
 		if (flowModel.getSearchParams() != null) {
 			String le = ((String[]) flowModel.getSearchParams().get("and_money_le"))[0].toString();
 			String ge = ((String[]) flowModel.getSearchParams().get("and_money_ge"))[0].toString();
@@ -76,5 +97,25 @@ public class Flow2Controller extends BaseController {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
+	public String checkMemberByName(){
+		HttpServletResponse response = ServletActionContext.getResponse();
+		PrintWriter out = null;
+	
+			try {
+				out = response.getWriter();
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+	
+			if(getMemberModel().getMemberByName(flowModel.getMemName())!=null){
+				out.print(1);
+			}else{
+				out.print(0);;
+			}
+			return null;
+	}
+	public String showgg(){
+		flowModel.getPageData("Flow");
+		return render("flow2/show");
+	}
 }
